e3f52f74487d9a8305ad1a737fb941e7fdc0a206
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-09-05 20:11:06 +0700

    init project

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import "dotenv/config.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`app listening in port ${port}`);
+  });
+});
