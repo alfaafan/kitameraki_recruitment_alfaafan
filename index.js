1d02549fc395ddcdf6524be7723f8682fc747347
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-09-05 20:23:18 +0700

    fix mongoDB connection url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017");
+    const conn = await mongoose.connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/kitameraki");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
