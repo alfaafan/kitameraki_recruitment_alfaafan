2cfdeb4fdd4d81e0bffcee2ecdc372b337b108e5
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-09-06 19:46:48 +0700

    feat(pagination): added get paginated task

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,13 +1,17 @@
 import { createTaskService } from "../services/createTask.service.js";
 import { deleteTaskService } from "../services/deleteTask.service.js";
+import { getPaginatedTasksService } from "../services/getPaginatedTasks.service.js";
 import { getTaskByIdService } from "../services/getTaskById.service.js";
 import { getTasksService } from "../services/getTasks.service.js";
 import { patchTaskService } from "../services/patchTask.service.js";
 import { error, success } from "../utils/apiResponse.js";
 
 export const getAllTasks = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 10;
+
   try {
-    const tasks = await getTasksService();
+    const tasks = await getPaginatedTasksService(page, pageSize);
     res.status(200).json(success("Tasks retrieved", tasks));
   } catch (e) {
     res.status(500).json(error(e.message));
