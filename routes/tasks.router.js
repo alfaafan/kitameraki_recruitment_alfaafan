e335730ce97289d7cc9c7fbb00199b80e3479e94
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-09-08 21:19:46 +0700

    feat(auth): added token verification middleware

--- routes/tasks.router.js
+++ routes/tasks.router.js
@@ -5,9 +5,9 @@ import { verifyToken } from "../middlewares/verifyToken.js";
 const tasksRouter = express.Router();
 
 tasksRouter.get("/", verifyToken, getAllTasks);
-tasksRouter.get("/:_id", getTaskById);
-tasksRouter.post("/", postTask);
-tasksRouter.patch("/:_id", patchTask);
-tasksRouter.delete("/:_id", deleteTask);
+tasksRouter.get("/:_id", verifyToken, getTaskById);
+tasksRouter.post("/", verifyToken, postTask);
+tasksRouter.patch("/:_id", verifyToken, patchTask);
+tasksRouter.delete("/:_id", verifyToken, deleteTask);
 
 export { tasksRouter };
