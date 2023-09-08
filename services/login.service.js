92e2039c8304f813b972ac6ba822017ac419b203
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-09-08 15:30:25 +0700

    fix(auth): fix jwt secret is not defined

--- services/login.service.js
+++ services/login.service.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const login = async (username, password) => {
   try {
@@ -16,6 +18,7 @@ export const login = async (username, password) => {
       return { success: false, message: "Wrong password" };
     }
 
+    const jwtSecret = process.env.JWT_SECRET;
     const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: "1h" });
 
     return { success: true, message: "Login success", data: { _id: user._id, username: user.username, token: token } };
