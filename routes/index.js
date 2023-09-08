75aa26838581c171b03d0fee07a4b3163c34d527
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-09-08 10:53:06 +0700

    feat(api-docs): added swagger ui for api documentation

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,18 @@
 import express from "express";
 import { tasksRouter } from "./tasks.router.js";
-import fs from "fs/promises";
-import { error } from "../utils/apiResponse.js";
+import fs from "fs";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
+import swaggerUi from "swagger-ui-express";
 
 const router = express.Router();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const swaggerDocumentPath = path.join(__dirname, "openapi.json");
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerDocumentPath, "utf8"));
 
 router.use("/tasks", tasksRouter);
-router.get("/openapi.json", async (req, res) => {
-  const openApiSpecPath = path.join(__dirname, "openapi.json");
-
-  try {
-    const data = await fs.readFile(openApiSpecPath, "utf8");
-
-    res.setHeader("Content-Type", "application/json");
-    res.send(data);
-  } catch (e) {
-    console.error("Error reading OpenAPI specification:", e);
-    res.status(500).json(error(e.message));
-  }
-});
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
