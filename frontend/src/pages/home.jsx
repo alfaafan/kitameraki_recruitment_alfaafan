f9e5495a897bad675c15b1b44bc3f972a78fb0e0
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-09-09 21:54:41 +0700

    feat(home): create home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Layout from "../layout/Layout";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <Layout>
-      <h1 className="text-6xl font-bold">Welcome to Task Management App!</h1>
+      <div className="flex flex-col min-h-screen justify-center items-center gap-8">
+        <h1 className="text-6xl font-bold text-center">Welcome to Task Management App!</h1>
+        <div className="flex gap-3">
+          <button className="btn btn-ghost">
+            <Link to={"/tasks"}>View tasks</Link>
+          </button>
+          <button className="btn btn-ghost">
+            <Link to={"/tasks/create"}>Create a new task</Link>
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 }
